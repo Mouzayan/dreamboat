bab1da37363d42d5d7621f42f2ac9a126c471793
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-05-14 15:07:51 -0400

    seed file fixes

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://dreamboat-api.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl,
 });
+
+
 
 // ========================================
 // ================ AUTH ==================
